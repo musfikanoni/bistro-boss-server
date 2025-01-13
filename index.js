0f8b879204ced83f5b0c7e3089e3ff68fff84d69
musfikanoni <musfikanoni@gmail.com>
2025-01-13 17:35:34 +0600

    users api

--- index.js
+++ index.js
@@ -27,11 +27,20 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db("bistroDB").collection("user");
     const menuCollection = client.db("bistroDB").collection("menu");
     const reviewCollection = client.db("bistroDB").collection("reviews");
     const cartCollection = client.db("bistroDB").collection("carts");
 
+    //user related api
+    app.post('/users', async(req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
 
+
+    //menu
     app.get('/menu', async(req,res) => {
         const result = await menuCollection.find().toArray();
         res.send(result);
